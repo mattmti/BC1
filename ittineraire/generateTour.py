e571d1cde27adbddcff564ff132af95883c60c56
mattmti <matteo.monti0907@gmail.com>
2026-05-29 10:48:45 +0100

    Add functions to calculate grouped cities and cities where an hotel will be

--- ittineraire/generateTour.py
+++ ittineraire/generateTour.py
@@ -7,7 +7,10 @@ citiesList = [["Tōkyō-to", 35.6768601, 139.7638947],
               ["Ōsaka-shi", 34.6937569, 135.5014539],
               ["Kanazawa-shi", 36.561627, 136.6568822],
               ["Chiba-shi", 35.6070629, 140.1062653],
-              ["Kyōto-shi", 35.0115754, 135.7681441]]
+              ["Kyōto-shi", 35.0115754, 135.7681441],
+              ["Nara-shi", 34.6845445, 135.8048359],
+              ["Kōya-chō", 34.215788, 135.5872944],
+              ["Himeji-shi", 34.8153529, 134.6854793]]
 
 
 def distanceBetween2cities(v1, v2):
@@ -43,13 +46,14 @@ def nearestNeighborPath(startCity, listCities):
     currentCity = startCity
     totalDistance = 0
 
-    
+
     while len(citiesList) > len(visited):
         nextCity, distance = nearestNeighbor(currentCity, listCities, visited)
         visited.append(nextCity[0])
         currentCity = nextCity
         totalDistance += distance
 
+    # We add the startCity because we need to go back to it at the end of the tour
     totalDistance += distanceBetween2cities(currentCity, startCity)
     visited.append(startCity[0])
     
@@ -59,7 +63,78 @@ def nearestNeighborPath(startCity, listCities):
     print(f"Distance totale = {totalDistance}")
 
 
-nearestNeighborPath(start, citiesList)
 
 
+
+def groupCities(listCities):
+    groupedCities = []
+    notgroupedcities = []
+    for i in range(len(listCities)):
+        newList = [listCities[i][0]]
+        for j in range(len(listCities)):
+            if listCities[i] != listCities[j]:
+                if distanceBetween2cities(listCities[i], listCities[j]) <= 50:
+                    newList.append(listCities[j][0])
+        if len(newList) >= 2 and not any(set(newList) == set(existing) for existing in groupedCities):
+            groupedCities.append(newList)
+        elif len(newList) == 1:
+            notgroupedcities.append(listCities[i][0])
+
+    return(groupedCities, notgroupedcities)
+
+
+citiesGrouped, citiesNotGrouped = (groupCities(citiesList))
+
+
+def findHotelCity(listGroupedCities, listCities):
+    listHotelCities = []
+    for i in range(len(listGroupedCities)):
+        minimum = 1000000
+        hotelCity = ""
+        for j in range(len(listGroupedCities[i])):
+            sum = 0
+            for k in range(len(listGroupedCities[i])):
+                if listGroupedCities[i][j] != listGroupedCities[i][k]:
+                    ville1 = None
+                    ville2 = None
+                    for element in listCities:
+                        if element[0] == listGroupedCities[i][j]:
+                            ville1 = element
+                        elif element[0] == listGroupedCities[i][k]:
+                            ville2 = element
+                    if ville1 and ville2:
+                        sum += distanceBetween2cities(ville1, ville2)
+        if sum < minimum:
+            minimum = sum
+            hotelCity = listGroupedCities[i][j]
+        listHotelCities.append(hotelCity)
+    
+    return(listHotelCities)
+
+hotelCitiesList = findHotelCity(citiesGrouped, citiesList) + citiesNotGrouped
+
+#print(citiesGrouped)
+#print(hotelCitiesList)
+
+
+
+def distanceInGroupedCities(listGroupedCities, listHotelCities, listCities):
+    totalDistance = 0
+    for i in range(len(listGroupedCities)):
+        hotelCity = listHotelCities[i]
+        for j in range(len(listGroupedCities[i])):
+            if listGroupedCities[i][j] != hotelCity:
+                ville1 = None
+                ville2 = None
+                for element in listCities:
+                    if element[0] == hotelCity:
+                        ville1 = element
+                    elif element[0] == listGroupedCities[i][j]:
+                        ville2 = element
+                if ville1 and ville2:
+                    totalDistance += distanceBetween2cities(ville1, ville2) * 2
+
+    return totalDistance
+
+#print(distanceInGroupedCities(citiesGrouped, hotelCitiesList, citiesList))
 
